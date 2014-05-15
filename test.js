2acd626711cfdc122dce6c844fecaf757e434268
Stephen Sawchuk <sawchuk@gmail.com>
2014-05-15 16:22:59 -0400

    fix #1 : default incorrect maxLength to longest word.

--- test.js
+++ test.js
@@ -11,3 +11,8 @@ it('should allow customization of line length', function () {
   var expected = '\n     _-----_\n    |       |    .----------.\n    |\u001b[31m--(o)--\u001b[39m|    |    Hi    |\n   `---------´   \'----------\'\n    \u001b[33m(\u001b[39m _\u001b[33m´U`\u001b[39m_ \u001b[33m)\u001b[39m    \n    /___A___\\    \n     \u001b[33m|  ~  |\u001b[39m     \n   __\u001b[33m\'.___.\'\u001b[39m__   \n ´   \u001b[31m`  |\u001b[39m° \u001b[31m´ Y\u001b[39m ` \n';
   assert.equal(yosay('Hi', { maxLength: 8 }), expected);
 });
+
+it('should override a maxLength setting that is too short', function () {
+  var expected = '\n     _-----_\n    |       |    .--------.\n    |\u001b[31m--(o)--\u001b[39m|    | Hello, |\n   `---------´   | buddy! |\n    \u001b[33m(\u001b[39m _\u001b[33m´U`\u001b[39m_ \u001b[33m)\u001b[39m    \'--------\'\n    /___A___\\    \n     \u001b[33m|  ~  |\u001b[39m     \n   __\u001b[33m\'.___.\'\u001b[39m__   \n ´   \u001b[31m`  |\u001b[39m° \u001b[31m´ Y\u001b[39m ` \n';
+  assert.equal(yosay('Hello, buddy!', { maxLength: 4 }), expected);
+});
